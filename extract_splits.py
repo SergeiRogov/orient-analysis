dd9a64d15c1d9842ba2ebca567d19470793618fb
Sergei Rogov <78348862+SergeiRogov@users.noreply.github.com>
2023-07-25 15:09:14 +0300

    Wrapped everything in a function

--- extract_splits.py
+++ extract_splits.py
@@ -17,148 +17,161 @@ class Runner:
                f"{self.overall_time} {self.bib} {self.course} {self.splits})"
 
 
-# Sequence of control points of every course
-all_courses_controls = []
-
-# List of lists of all runners
-all_courses_runners = []
-
-# Regular expression for a control point "3(57)"
-control_point = re.compile(r'^\d{1,2}\(\d{2,3}\)\s*')
-
-# Regular expression for a course title "Blue (16)"
-course_title = re.compile(r'\w+\s*\(\d{2,3}\)\s*')
-
-# Regular expression for time "03:22"
-split_time = re.compile(r'\d{1,2}:\d{2}')
-
-# Read the HTML file
-with open('Sia Mathiatis 26 Mar 2023 splits2.html', 'r') as file:
-    html_content = file.read()
-
-# Create BeautifulSoup object
-soup = BeautifulSoup(html_content, 'html.parser')
-
-
-# Find the title of event
-title = soup.find('title')
-
-
 def has_child_i_tag(tag):
     return tag.find('i', recursive=False) is not None
 
 
-# Iterate over each line of the document and delete empty tags
-for x in soup.find_all():
-    if len(x.get_text(strip=True)) == 0 and x.name not in ['font'] and len(x.find_all()) == 0 \
-            or has_child_i_tag(x):
-        x.extract()
+def extract_splits(html_file):
+    # Sequence of control points of every course
+    all_courses_controls = []
 
-# Names of courses
-courses = soup.find_all('b', string=course_title)
-courses = map(lambda title: title.text.strip(),  courses)
-courses = list(map(lambda title: re.sub(r"\s*\(\d+\)", '', title),  courses))
+    # List of lists of all runners
+    all_courses_runners = []
 
-# Find the table elements containing the results for all 3 courses (and header coming first)
-tables = soup.find_all('table')
+    # Regular expression for a control point "3(57)"
+    control_point = re.compile(r'^\d{1,2}\(\d{2,3}\)\s*')
 
-# Iterating through 3 courses (=tables) and extracting splits
-for course_index, table in enumerate(tables[1:]):
+    # Regular expression for a course title "Blue (16)"
+    course_title = re.compile(r'\w+\s*\(\d{2,3}\)\s*')
 
-    # Control points of a course
-    course_controls = []
+    # Regular expression for time "03:22"
+    split_time = re.compile(r'\d{1,2}:\d{2}')
 
-    # Runners of a course
-    course_runners = []
+    # Read the HTML file
+    with open(html_file, 'r') as file:
+        html_content = file.read()
 
-    runner_splits = []
-    place_num = 0
-    bib = 0
-    name = ''
-    group = ''
-    time = ''
-    info_row = None
-    num_of_control_rows = None
-    is_runner_extracted = False
-    # extract_name_mode = False
-    num_of_font_cells = 0
+    # Create BeautifulSoup object
+    soup = BeautifulSoup(html_content, 'html.parser')
 
-    # Extract table's rows
-    rows = table.find_all('tr')
+    # Find the title of event
+    title = soup.find('title').text.strip()
 
-    # Process each row
-    for row_index, row in enumerate(rows):
-        print(row_index)
-        # Skip redundant row (with separated splits)
-        if info_row and num_of_control_rows and \
-                (info_row & 1) != (row_index & 1) and row_index - info_row < num_of_control_rows * 2 - 1:
-            continue
+    # Iterate over each line of the document and delete empty tags
+    for x in soup.find_all():
+        if len(x.get_text(strip=True)) == 0 and x.name not in ['font'] and len(x.find_all()) == 0 \
+                or has_child_i_tag(x):
+            x.extract()
 
-        is_runner_found = False
-        runner_info_start_index = None
+    # Names of courses
+    courses = soup.find_all('b', string=course_title)
+    courses = map(lambda title: title.text.strip(),  courses)
+    courses = list(map(lambda title: re.sub(r"\s*\(\d+\)", '', title),  courses))
 
-        # Extract row's cells
-        cells = row.find_all('td')
+    # Find the table elements containing the results for all 3 courses (and header coming first)
+    tables = soup.find_all('table')
 
-        # Process each cell
-        for cell_index, cell in enumerate(cells):
-            print(cell)
-            # Extracting course controls' sequence
-            if cell.find(string=control_point) or cell.text.strip() == "F":
-                course_controls.append(cell.text.strip())
-                if cell.text.strip() == "F":
-                    # Number of rows covering a course
-                    # if it's a single row course (row_index = 0) - it still would be a 2 row course in splits
-                    num_of_control_rows = row_index + 1 if row_index > 0 else 2
+    # Iterating through 3 courses (=tables) and extracting splits
+    for course_index, table in enumerate(tables[1:]):
 
-            # Finding first field related to runner (first <font> tag)
-            elif not is_runner_found and cell.find('font'):
-                is_runner_found = True
-                is_runner_extracted = False
-                runner_splits = []
+        # Control points of a course
+        course_controls = []
 
-                runner_info_start_index = cell_index
-                info_row = row_index
-                extract_name_mode = True
+        # Runners of a course
+        course_runners = []
 
-                num_of_font_cells = 1
-                i = cell_index
-                while cells[i + 1].find('font'):
-                    num_of_font_cells += 1
-                    i += 1
+        runner_splits = []
+        place_num = 0
+        bib = 0
+        name = ''
+        group = ''
+        time = ''
+        info_row = None
+        num_of_control_rows = None
+        is_runner_extracted = False
+        num_of_font_cells = 0
 
-                # Recording info from this field
-                if num_of_font_cells == 5:
-                    place_num = cell.text.strip()
-                else:
-                    place_num = "<-->"
-                    bib = cell.text.strip()
+        # Extract table's rows
+        rows = table.find_all('tr')
 
-            # Recording runners' info from the 4 following fields
-            # Recording splits from the same row
-            elif is_runner_found and runner_info_start_index is not None:
-                if cell_index < num_of_font_cells:
+        # Process each row
+        for row_index, row in enumerate(rows):
+            # Skip redundant row (with separated splits)
+            if info_row and num_of_control_rows and \
+                    (info_row & 1) != (row_index & 1) and row_index - info_row < num_of_control_rows * 2 - 1:
+                continue
+
+            is_runner_found = False
+            runner_info_start_index = None
+
+            # Extract row's cells
+            cells = row.find_all('td')
+
+            # Process each cell
+            for cell_index, cell in enumerate(cells):
+                # Extracting course controls' sequence
+                if cell.find(string=control_point) or cell.text.strip() == "F":
+                    course_controls.append(cell.text.strip())
+                    if cell.text.strip() == "F":
+                        # Number of rows covering a course
+                        # if it's a single row course (row_index = 0) - it still would be a 2 row course in splits
+                        num_of_control_rows = row_index + 1 if row_index > 0 else 2
+
+                # Finding first field related to runner (first <font> tag)
+                elif not is_runner_found and cell.find('font'):
+                    is_runner_found = True
+                    is_runner_extracted = False
+                    runner_splits = []
+
+                    runner_info_start_index = cell_index
+                    info_row = row_index
+                    extract_name_mode = True
+
+                    num_of_font_cells = 1
+                    i = cell_index
+                    while cells[i + 1].find('font'):
+                        num_of_font_cells += 1
+                        i += 1
+
+                    # Recording info from this field
                     if num_of_font_cells == 5:
-                        if cell_index == runner_info_start_index + 1:
-                            bib = cell.text.strip() if cell.text.strip() != '' else "<-->"
-                        elif cell_index == runner_info_start_index + 2:
-                            name = cell.text.strip() if cell.text.strip() != '' else "<-->"
-                        elif cell_index == runner_info_start_index + 3:
-                            group = cell.text.strip() if cell.text.strip() != '' else "<-->"
-                        elif cell_index == runner_info_start_index + 4:
-                            time = cell.text.strip() if cell.text.strip() != '' else "<-->"
+                        place_num = cell.text.strip()
                     else:
-                        if cell_index == runner_info_start_index + 1:
-                            name = cell.text.strip() if cell.text.strip() != '' else "<-->"
-                        elif cell_index == runner_info_start_index + 2:
-                            group = cell.text.strip() if cell.text.strip() != '' else "<-->"
-                        elif cell_index == runner_info_start_index + 3:
-                            time = cell.text.strip() if cell.text.strip() != '' else "<-->"
-                # splits
-                elif cell_index >= num_of_font_cells and len(runner_splits) < len(course_controls):
+                        place_num = "<-->"
+                        bib = cell.text.strip()
+
+                # Recording runners' info from the 4 following fields
+                # Recording splits from the same row
+                elif is_runner_found and runner_info_start_index is not None:
+                    if cell_index < num_of_font_cells:
+                        if num_of_font_cells == 5:
+                            if cell_index == runner_info_start_index + 1:
+                                bib = cell.text.strip() if cell.text.strip() != '' else "<-->"
+                            elif cell_index == runner_info_start_index + 2:
+                                name = cell.text.strip() if cell.text.strip() != '' else "<-->"
+                            elif cell_index == runner_info_start_index + 3:
+                                group = cell.text.strip() if cell.text.strip() != '' else "<-->"
+                            elif cell_index == runner_info_start_index + 4:
+                                time = cell.text.strip() if cell.text.strip() != '' else "<-->"
+                        else:
+                            if cell_index == runner_info_start_index + 1:
+                                name = cell.text.strip() if cell.text.strip() != '' else "<-->"
+                            elif cell_index == runner_info_start_index + 2:
+                                group = cell.text.strip() if cell.text.strip() != '' else "<-->"
+                            elif cell_index == runner_info_start_index + 3:
+                                time = cell.text.strip() if cell.text.strip() != '' else "<-->"
+                    # splits
+                    elif cell_index >= num_of_font_cells and len(runner_splits) < len(course_controls):
+                        if cell.find(string=split_time):
+                            runner_splits.append(cell.text.strip())
+                            # if splits end on the same row they start - add a runner
+                            if len(runner_splits) == len(course_controls):
+                                # Adding runner info
+                                course_runners.append(
+                                    Runner(name, courses[course_index], place_num, bib, group, time, runner_splits))
+                                is_runner_extracted = True
+                                break
+                        else:
+                            # Adding not fully completed runner info (splits are not right)
+                            course_runners.append(
+                                Runner(name, courses[course_index], place_num, bib, group, time, runner_splits))
+                            is_runner_extracted = True
+                            break
+
+                # Adding splits from other rows with same parity
+                elif (info_row & 1) == (row_index & 1) and info_row != row_index:
                     if cell.find(string=split_time):
                         runner_splits.append(cell.text.strip())
-                        # if splits end on the same row they start - add a runner
                         if len(runner_splits) == len(course_controls):
                             # Adding runner info
                             course_runners.append(
@@ -167,32 +180,21 @@ for course_index, table in enumerate(tables[1:]):
                             break
                     else:
                         # Adding not fully completed runner info (splits are not right)
-                        course_runners.append(
-                            Runner(name, courses[course_index], place_num, bib, group, time, runner_splits))
-                        is_runner_extracted = True
-                        break
+                        if not is_runner_extracted:
+                            course_runners.append(
+                                Runner(name, courses[course_index], place_num, bib, group, time, runner_splits))
+                            is_runner_extracted = True
+                            break
 
-            # Adding splits from other rows with same parity
-            elif (info_row & 1) == (row_index & 1) and info_row != row_index:
-                if cell.find(string=split_time):
-                    runner_splits.append(cell.text.strip())
-                    if len(runner_splits) == len(course_controls):
-                        # Adding runner info
-                        course_runners.append(
-                            Runner(name, courses[course_index], place_num, bib, group, time, runner_splits))
-                        is_runner_extracted = True
-                        break
-                else:
-                    # Adding not fully completed runner info (splits are not right)
-                    if not is_runner_extracted:
-                        course_runners.append(
-                            Runner(name, courses[course_index], place_num, bib, group, time, runner_splits))
-                        is_runner_extracted = True
-                        break
+        all_courses_controls.append(course_controls)
+        all_courses_runners.append(course_runners)
 
-    all_courses_controls.append(course_controls)
-    all_courses_runners.append(course_runners)
+    # for i in range(len(all_courses_runners)):
+    #     for j in range(len(all_courses_runners[i])):
+    #         print(repr(all_courses_runners[i][j]))
 
-for i in range(len(all_courses_runners)):
-    for j in range(len(all_courses_runners[i])):
-        print(repr(all_courses_runners[i][j]))
+    return title, all_courses_controls, all_courses_runners, courses
+
+
+html_file = 'Sia Mathiatis 26 Mar 2023 splits2.html'
+print(extract_splits(html_file))
