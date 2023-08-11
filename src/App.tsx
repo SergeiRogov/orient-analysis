340f2e876bba9a7b4cf23042e035e85a558357fe
Sergei Rogov <78348862+SergeiRogov@users.noreply.github.com>
2023-08-11 21:56:27 +0300

    Nested menu almost formed

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect, createContext } from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import './App.css';
 import { Navbar } from './components/Navbar';
+import { Dropdown } from './components/Dropdown';
 import { Graphs } from './pages/Graphs';
 import { SplitsTable } from './pages/SplitsTable';
 import axios from 'axios';
-import { Dropdown,  } from './components/Dropdown';
 
 export interface Option {
   label: string;
@@ -29,10 +29,6 @@ interface Props {
   runners: Runner[][];
 }
 
-// interface Splits {
-//   data: Props;
-// }
-
 const options: Option[] = [
   { label: 'Kato Drys 19 Jan 2023', value: 'Kato Drys 19 Jan 2023.html' },
   { label: 'Kouris Dam 20 Feb 2023', value: 'Kouris Dam 20 Feb 2023.html' },
@@ -46,6 +42,7 @@ function App() {
 
   const [orientEvent, setOrientEvent] = useState<string>(options[options.length - 1].value);
   const [splitData, setSplitData] = useState<Props | undefined>(undefined);
+  
   useEffect(() => {
     // API Gateway endpoint 
     axios.get('https://8cb9vtn6xb.execute-api.eu-west-3.amazonaws.com/Stage1/extract-orienteering-splits', {
@@ -65,7 +62,10 @@ function App() {
     <div className="App">
       <AppContext.Provider value={ splitData }>
       <Router>
-      <Dropdown options={options} setOrientEvent={setOrientEvent} />
+      <Dropdown 
+        options={ options } 
+        orientEvent={ orientEvent } 
+        setOrientEvent={ setOrientEvent } />
       <Navbar/>
         <Routes>
           <Route path="/" element={<SplitsTable />}/>
--- src/Dropdown.tsx
+++ src/Dropdown.tsx
@@ -1,28 +1,53 @@
-import React, { useState, useEffect, createContext } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { Option } from "../App"
 import axios from 'axios'
+import { AppContext } from "../App"
 
 interface Props {
     options: Option[];
+    orientEvent: string;
     setOrientEvent: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export const Dropdown = ({options, setOrientEvent}: Props) => {
+export const Dropdown = ({options, orientEvent, setOrientEvent}: Props) => {
+    const appContext = useContext(AppContext);
+
+    if (!appContext) {
+        // Handle when the context is undefined (optional)
+        return <div>Loading...</div>;
+    }
+
+    const splitData = appContext;
+
+    const [orientCourse, setOrientCourse] = useState<string>(splitData.courses[0]);
+    
     return (
-        <div className="menu">
-            <div className="menu">
-                <h3>Choose event:</h3>
+        <div className="Event-Course-Menu">
+            <h3>Select an event:</h3>
+            <select 
+                className="form-select" 
+                defaultValue={ orientEvent }
+                onChange={(event) => setOrientEvent(event.target.value)}>
+                {options.map((opt: Option) => (
+                    <option value={opt.value}>{ opt.label }</option>
+                ))}
+            </select>
+
+            { orientEvent &&(
+            <div className="CourseMenu">
+                <h3>Select a Course:</h3>
                 <select 
                     className="form-select" 
-                    defaultValue={options[options.length - 1].value}
-                    onChange={(event) => setOrientEvent(event.target.value)}>
-                    {options.map((opt: Option) => (
-                        <option value={opt.value}>{opt.label}</option>
+                    defaultValue={ orientCourse }
+                    onChange={(event) => setOrientCourse(event.target.value)}>
+                    {splitData.courses.map((course: string) => (
+                        <option>{ course }</option>
                     ))}
                 </select>
             </div>
+            )}
         </div>
-    )
+    );
 }
 
 export {}
